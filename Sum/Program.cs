32604931a169caface13e2b7ce73092734d32232
Marina <m.a.dedova@gmail.com>
2023-02-24 23:51:27 +0300

    random

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31; //create first number
-int numberB = 5; //create second number
+﻿int numberA = new Random().Next(1,10); //1 2 3 4 5 6...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); //1 2 3 4 5 6...9
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
