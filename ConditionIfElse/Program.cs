57f36de115abe39615cde4b38e04d7e792a4d50a
Marina <m.a.dedova@gmail.com>
2023-02-25 12:00:05 +0300

    special user

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("put the user name:");
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+  Console.WriteLine("Hooray,it's Masha!"); 
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
